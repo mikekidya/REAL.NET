b4355e29853f420bb7ebd43e50a5cb2dd6b462bd
Mikhail Kidyankin <kidyankin@icloud.com>
2018-09-29 15:19:08 +0300

    Sample: data classes added

---
+++ SampleDataflowProject/City.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataflowProject
+{
+    public class City
+    {
+        private readonly String name;
+        private bool isHasAirport = false;
+        private LinkedList<Road> railwayRoads;
+        private LinkedList<Road> airwayRoads;
+
+        public City(String name = "No name")
+        {
+            this.name = name;
+            railwayRoads = new LinkedList<Road>();
+        }
+
+        public void AddAirwayRoad(City destination, int cost)
+        {
+            Road road = new Road(Road.RoadType.Airway, cost, this, destination);
+            if (!HasAirport())
+            {
+                isHasAirport = true;
+                airwayRoads = new LinkedList<Road>();
+            }
+            airwayRoads.AddLast(road);
+        }
+
+        public void AddRailwayRoad(City destination, int cost)
+        {
+            railwayRoads.AddLast(new Road(Road.RoadType.Railway, cost, this, destination));
+        }
+
+        public bool HasAirport()
+        {
+            return isHasAirport;
+        }
+
+        public LinkedList<Road> GetAirwayRoads()
+        {
+            return airwayRoads;
+        }
+
+        public LinkedList<Road> GetRailwayRoads()
+        {
+            return railwayRoads;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+    }
+}
---
+++ SampleDataflowProject/Road.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataflowProject
+{
+    public class Road
+    {
+        public enum RoadType { Railway, Airway };
+
+        private readonly RoadType roadType;
+        private readonly int cost;
+        private readonly City departCity;
+        private readonly City destinationCity;
+
+        public Road(RoadType roadType, int cost, City departCity, City destinationCity)
+        {
+            this.roadType = roadType;
+            this.cost = cost;
+            this.departCity = departCity;
+            this.destinationCity = destinationCity;
+        }
+
+        public RoadType GetRoadType()
+        {
+            return roadType;
+        }
+
+        public City GetDepartationCity()
+        {
+            return departCity;
+        }
+
+        public City GetDestinationCity()
+        {
+            return destinationCity;
+        }
+    }
+}
