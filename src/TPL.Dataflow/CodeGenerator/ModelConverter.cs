d3a0515fb11d33cb4a5d37bf5cdbfd3d4cc0f48b
Mikhail Kidyankin <kidyankin@icloud.com>
2018-11-17 13:43:24 +0300

    Form added in CodeGenerator proj

---
+++ src/TPL.Dataflow/CodeGenerator/ModelConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator
+{
+	using Repo;
+
+	class ModelConverter
+	{
+		public Model getModelFromRepo()
+		{
+			//IModel model = Model("DataflowTestModel");
+
+			return null;
+		}
+	}
+}
