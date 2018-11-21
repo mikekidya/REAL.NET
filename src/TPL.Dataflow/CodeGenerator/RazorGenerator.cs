c3d9fef08ff670c24e8f7e024f837d5edc9b84c1
Mikhail Kidyankin <kidyankin@icloud.com>
2018-11-22 00:57:06 +0300

    Target changed to .NET Framework 4.7.2, dependencies fixed, crashes fixed

--- src/TPL.Dataflow/CodeGenerator/RazorGenerator.cs
+++ src/TPL.Dataflow/CodeGenerator/RazorGenerator.cs
@@ -8,8 +8,15 @@ namespace CodeGenerator
         public static string GenerateFromRepoModel(IModel repoModel)
         {
             var model = ModelConverter.ConvertModelFromRepo(repoModel);
+            /*var razorEngine = new RazorLightEngineBuilder()
+                .UseFilesystemProject(System.IO.Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory))
+                .UseMemoryCachingProvider()
+                .Build();
+
+            string result = razorEngine.CompileRenderAsync("template.cshtml", model).Result;*/
             var razorEngine = EngineFactory.CreatePhysical(System.IO.Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory));
-            return razorEngine.Parse("template.cshtml", model);
+            var result = razorEngine.Parse("template.cshtml", model);
+            return result;
         }
     }
 }
