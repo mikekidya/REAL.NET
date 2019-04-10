5ed230297e2556b703d8604d10cd2ce2376364f9
Mikhail Kidyankin <kidyankin@icloud.com>
2019-04-10 17:32:34 +0300

    First request on facade added

---
+++ src/RepoAPI/Controllers/RepoFacadeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using Repo;
+using RepoAPI.Models;
+
+namespace RepoAPI.Controllers
+{ 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepoFacadeController : ControllerBase
+    { 
+        [HttpGet("models")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var models = RepoContainer.CurrentRepo().Models;
+            return models.ToList().ConvertAll<string>((model) => model.Name);
+        }
+    }
+}
---
+++ src/RepoAPI/Models/RepoContainer.cs
@@ -0,0 +1,29 @@
+﻿using Repo;
+
+namespace RepoAPI.Models
+{
+    public static class RepoContainer
+    {
+        public static IRepo repo { get; private set; }
+
+        public static void Create()
+        {
+            repo = RepoFactory.Create();
+        }
+
+        public static void Load(string path)
+        {
+            repo = RepoFactory.Load(path);
+        }
+
+        public static IRepo CurrentRepo()
+        {
+            if (repo is null)
+            {
+                Create();
+            }
+            return repo;
+        }
+
+    }
+}
