49e1ae57c9550c4918c4904493f0514e4a3e3d98
Mikhail Kidyankin <kidyankin@icloud.com>
2019-05-06 13:03:55 +0300

    Sptil Controllers

--- src/RepoAPI/Controllers/ElementController.cs
+++ src/RepoAPI/Controllers/ElementController.cs
@@ -8,62 +8,24 @@ using Repo;
 using RepoAPI.Models;
 
 namespace RepoAPI.Controllers
-{ 
+{
+    /// <summary>
+    /// Element controller is used to create, get, change and remove elements.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    public class RepoFacadeController : ControllerBase
+    public class ElementController : ControllerBase
     {
         private readonly IMapper _mapper;
-        private readonly object lockObject = new Object();
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:RepoAPI.Controllers.RepoFacadeController"/> class.
+        /// Initializes a new instance of the <see cref="T:RepoAPI.Controllers.ElementController"/> class.
         /// </summary>
         /// <param name="mapper">Mapper is used to map object from Repo to Model classes.</param>
-        public RepoFacadeController(IMapper mapper)
+        public ElementController(IMapper mapper)
         {
             _mapper = mapper;
         }
-
-        /// <summary>
-        /// Return all visible model names in repository.
-        /// </summary>
-        /// <returns>The sequence of models names.</returns>
-        [HttpGet("models")]
-        public ActionResult<IEnumerable<string>> GetModels() =>
-            RepoContainer.CurrentRepo()
-            .Models
-            .ToList()
-            .ConvertAll<string>(model => model.Name);
-
-        /// <summary>
-        /// Returns model by its name.
-        /// </summary>
-        /// <returns>Model by its name.</returns>
-        /// <param name="modelName">Model name.</param>
-        [HttpGet("model/{modelName}")]
-        public ActionResult<Model> Model(string modelName) =>
-            _mapper.Map<Model>(RepoContainer.CurrentRepo().Model(modelName));
-
-        /// <summary>
-        /// Returns the node in model specified by its unique number key.
-        /// </summary>
-        /// <returns>The node.</returns>
-        /// <param name="modelName">Model name.</param>
-        /// <param name="id">Number key</param>
-        [HttpGet("model/{modelName}/node/{id}")]
-        public ActionResult<Node> GetNode(string modelName, int id) =>
-            _mapper.Map<Node>((INode) GetElementFromRepo(modelName, id));
-
-        /// <summary>
-        /// Returns the edge in model specified by its unique number key.
-        /// </summary>
-        /// <returns>The edge.</returns>
-        /// <param name="modelName">Model name.</param>
-        /// <param name="id">Number key.</param>
-        [HttpGet("model/{modelName}/edge/{id}")]
-        public ActionResult<Edge> GetEdge(string modelName, int id) => 
-            _mapper.Map<Edge>((IEdge) GetElementFromRepo(modelName, id));
 
         /// <summary>
         /// Returns the element in model specified by its unique number key.
@@ -71,50 +33,31 @@ namespace RepoAPI.Controllers
         /// <returns>The element.</returns>
         /// <param name="modelName">Model name.</param>
         /// <param name="id">Number key.</param>
-        [HttpGet("model/{modelName}/element/{id}")]
-        public ActionResult<Element> GetElement(string modelName, int id) => 
+        [HttpGet("{modelName}/{id}")]
+        public ActionResult<Element> GetElement(string modelName, int id) =>
             _mapper.Map<Element>(GetElementFromRepo(modelName, id));
 
         /// <summary>
-        /// Creates new model from metamodel.
+        /// Returns the node in model specified by its unique number key.
         /// </summary>
-        /// <param name="metamodel">Metamodel name.</param>
-        /// <param name="name">Name from new model. Should be unique.</param>
-        [HttpPost("model/create/{metamodel}/{name}")]
-        public void CreateModel(string metamodel, string name)
-        {
-            lock (lockObject)
-            {
-                RepoContainer.CurrentRepo().CreateModel(name, metamodel);
-            }
-        }
-
-        /// <summary>
-        /// Removes model from repository.
-        /// </summary>
+        /// <returns>The node.</returns>
         /// <param name="modelName">Model name.</param>
-        [HttpDelete("model/{modelName}")]
-        public void DeleteModel(string modelName)
-        {
-            lock (lockObject)
-            {
-                IModel model = GetModelFromRepo(modelName);
-                RepoContainer.CurrentRepo().DeleteModel(model);
-            }
-        }
+        /// <param name="id">Number key</param>
+        [HttpGet("{modelName}/{id}/asNode")]
+        public ActionResult<Node> GetNode(string modelName, int id) =>
+            _mapper.Map<Node>((INode)GetElementFromRepo(modelName, id));
+
 
         /// <summary>
-        /// Saves repository into file.
+        /// Returns the edge in model specified by its unique number key.
         /// </summary>
-        /// <param name="path">Path of the file.</param>
-        [HttpPost("save/{path}")]
-        public void SaveRepo(string path)
-        {
-            lock (lockObject)
-            {
-                RepoContainer.CurrentRepo().Save(path);
-            }
-        }
+        /// <returns>The edge.</returns>
+        /// <param name="modelName">Model name.</param>
+        /// <param name="id">Number key.</param>
+        [HttpGet("{modelName}/{id}/asEdge")]
+        public ActionResult<Edge> GetEdge(string modelName, int id) =>
+            _mapper.Map<Edge>((IEdge)GetElementFromRepo(modelName, id));
+
 
         /// <summary>
         /// Creates new element in model by its parent.
@@ -122,10 +65,10 @@ namespace RepoAPI.Controllers
         /// <returns>The element.</returns>
         /// <param name="modelName">Model name.</param>
         /// <param name="parentId">Parent identifier.</param>
-        [HttpPost("model/{modelName}/element/create/{parentId}")]
+        [HttpPost("{modelName}/{parentId}")]
         public ActionResult<int> CreateElement(string modelName, int parentId)
         {
-            lock (lockObject)
+            lock (Locker.obj)
             {
                 //throw new NullReferenceException();
                 IModel meta = GetModelFromRepo(modelName).Metamodel;
@@ -135,46 +78,19 @@ namespace RepoAPI.Controllers
             }
         }
 
-        /// <summary>
-        /// Changes the name of the model.
-        /// </summary>
-        /// <param name="modelName">Model name.</param>
-        /// <param name="newName">New name.</param>
-        [HttpPut("model/{modelName}/name/{newName}")]
-        public void ChangeModelName(string modelName, string newName)
-        {
-            lock (lockObject)
-            {
-                GetModelFromRepo(modelName).Name = newName;
-            }
-        }
 
         /// <summary>
-        /// Changes the model visability.
-        /// </summary>
-        /// <param name="modelName">Model name.</param>
-        /// <param name="isVisible">New visability value (true/fasle).</param>
-        [HttpPut("model/{modelName}/visability/{isVisible}")]
-        public void ChangeModelVisability(string modelName, bool isVisible)
-        {
-            lock (lockObject)
-            {
-                GetModelFromRepo(modelName).IsVisible = isVisible;
-            }
-        }
-
-        /// <summary>
-        /// Removes the element from model.
+        /// Changes the name of the element.
         /// </summary>
         /// <param name="modelName">Model name.</param>
         /// <param name="elementId">Element identifier.</param>
-        [HttpDelete("model/{modelName}/element/{elementId}")]
-        public void DeleteElement(string modelName, int elementId)
+        /// <param name="newName">New name.</param>
+        [HttpPut("{modelName}/{elementId}/name/{newName}")]
+        public void ChangeElementName(string modelName, int elementId, string newName)
         {
-            lock (lockObject)
+            lock (Locker.obj)
             {
-                GetModelFromRepo(modelName).DeleteElement(
-                    GetElementFromRepo(modelName, elementId));
+                GetElementFromRepo(modelName, elementId).Name = newName;
             }
         }
 
@@ -184,12 +100,12 @@ namespace RepoAPI.Controllers
         /// <param name="modelName">Model name.</param>
         /// <param name="edgeId">Edge identifier.</param>
         /// <param name="elementId">"From" element identifier.</param>
-        [HttpPut("model/{modelName}/edge/{edgeId}/from/{elementId}")]
+        [HttpPut("{modelName}/{edgeId}/from/{elementId}")]
         public void ChangeEdgeFrom(string modelName, int edgeId, int elementId)
         {
-            lock (lockObject)
+            lock (Locker.obj)
             {
-                ((IEdge) GetElementFromRepo(modelName, edgeId)).From =
+                ((IEdge)GetElementFromRepo(modelName, edgeId)).From =
                     GetElementFromRepo(modelName, elementId);
             }
         }
@@ -200,30 +116,16 @@ namespace RepoAPI.Controllers
         /// <param name="modelName">Model name.</param>
         /// <param name="edgeId">Edge identifier.</param>
         /// <param name="elementId">"To" element identifier.</param>
-        [HttpPut("model/{modelName}/edge/{edgeId}/to/{elementId}")]
+        [HttpPut("{modelName}/{edgeId}/to/{elementId}")]
         public void ChangeEdgeTo(string modelName, int edgeId, int elementId)
         {
-            lock (lockObject)
+            lock (Locker.obj)
             {
                 ((IEdge)GetElementFromRepo(modelName, edgeId)).To =
                     GetElementFromRepo(modelName, elementId);
             }
         }
 
-        /// <summary>
-        /// Changes the name of the element.
-        /// </summary>
-        /// <param name="modelName">Model name.</param>
-        /// <param name="elementId">Element identifier.</param>
-        /// <param name="newName">New name.</param>
-        [HttpPut("model/{modelName}/element/{elementId}/name/{newName}")]
-        public void ChangeElementName(string modelName, int elementId, string newName)
-        {
-            lock (lockObject)
-            {
-                GetElementFromRepo(modelName, elementId).Name = newName;
-            }
-        }
 
         /// <summary>
         /// Adds the attribute into element.
@@ -233,15 +135,15 @@ namespace RepoAPI.Controllers
         /// <param name="attributeName">Attribute name.</param>
         /// <param name="attributeKind">Attribute kind.</param>
         /// <param name="defaultValue">Default value.</param>
-        [HttpPost("model/{modelName}/element/{elementId}/attribute/{attributeName}/{attributeKind}/{defaultValue}")]
-        public void AddAttribute(string modelName, int elementId, 
+        [HttpPost("{modelName}/{elementId}/attribute/{attributeName}/{attributeKind}/{defaultValue}")]
+        public void AddAttribute(string modelName, int elementId,
             string attributeName, Models.AttributeKind attributeKind, string defaultValue)
         {
-            lock (lockObject)
+            lock (Locker.obj)
             {
                 GetElementFromRepo(modelName, elementId).AddAttribute(
-                    attributeName, 
-                    _mapper.Map<Repo.AttributeKind>(attributeKind), 
+                    attributeName,
+                    _mapper.Map<Repo.AttributeKind>(attributeKind),
                     defaultValue);
             }
         }
@@ -253,11 +155,11 @@ namespace RepoAPI.Controllers
         /// <param name="elementId">Element identifier.</param>
         /// <param name="attributeName">Attribute name.</param>
         /// <param name="newValue">New value.</param>
-        [HttpPut("model/{modelName}/element/{elementId}/attribute/{attributeName}/value/{newValue}")]
+        [HttpPut("{modelName}/{elementId}/attribute/{attributeName}/value/{newValue}")]
         public void ChangeAttributeValue(string modelName, int elementId,
             string attributeName, string newValue)
         {
-            lock (lockObject)
+            lock (Locker.obj)
             {
                 //var infrastructure = new InfrastructureSemantic(RepoContainer.CurrentRepo());
                 //var elem = GetElementFromRepo(modelName, elementId);
@@ -277,17 +179,32 @@ namespace RepoAPI.Controllers
         /// <param name="elementId">Element identifier.</param>
         /// <param name="attributeName">Attribute name.</param>
         /// <param name="newReference">New reference element identifier.</param>
-        [HttpPut("model/{modelName}/element/{elementId}/attribute/{attributeName}/reference/{newReference}")]
+        [HttpPut("{modelName}/{elementId}/attribute/{attributeName}/reference/{newReference}")]
         public void ChangeAttributeReference(string modelName, int elementId,
             string attributeName, int newReference)
         {
-            lock (lockObject)
+            lock (Locker.obj)
             {
                 GetElementFromRepo(modelName, elementId)
                     .Attributes
                     .Where(attribute => (attribute.Name == attributeName))
                     .First()
                     .ReferenceValue = GetElementFromRepo(modelName, newReference);
+            }
+        }
+
+        /// <summary>
+        /// Removes the element from model.
+        /// </summary>
+        /// <param name="modelName">Model name.</param>
+        /// <param name="elementId">Element identifier.</param>
+        [HttpDelete("{modelName}/{elementId}")]
+        public void DeleteElement(string modelName, int elementId)
+        {
+            lock (Locker.obj)
+            {
+                GetModelFromRepo(modelName).DeleteElement(
+                    GetElementFromRepo(modelName, elementId));
             }
         }
 
